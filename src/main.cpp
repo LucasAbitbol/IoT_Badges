1bbe43319bb865f4e5d88dc291a05b07814c6c31
LucasAbitbol <lucasabitbol6@gmail.com>
2022-02-18 11:58:17 +0100

    Lecteur de carte fonctionnel

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,61 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
-#include "Arduino.h"
+#include <SPI.h>
+#include <MFRC522.h>
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
-// #define LED_BUILTIN 13
+#define SS_PIN D2
+#define RST_PIN D1
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance.
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+int readsuccess;
+byte readcard[4];
+char str[32] = "";
+String StrUID;
+
+void setup() {
+  Serial.begin(115200); // Initialize serial communications with the PC
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init(); // Init MFRC522 card
+  Serial.println("Scan PICC to see UID...");
+  Serial.println("");
+}
+// --------------------------------------------------------------------
+// --------------------------------------------------------------------
+void array_to_string(byte array[], unsigned int len, char buffer[]) {
+    for (unsigned int i = 0; i < len; i++)
+    {
+        byte nib1 = (array[i] >> 4) & 0x0F;
+        byte nib2 = (array[i] >> 0) & 0x0F;
+        buffer[i*2+0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
+        buffer[i*2+1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
+    }
+    buffer[len*2] = '\0';
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+int getid() {  
+  if(!mfrc522.PICC_IsNewCardPresent()) {
+    return 0;
+  }
+  if(!mfrc522.PICC_ReadCardSerial()) {
+    return 0;
+  }
+ 
+  
+  Serial.println("THE UID OF THE SCANNED CARD IS:");
+  
+  for(int i=0;i<4;i++){
+    readcard[i]=mfrc522.uid.uidByte[i]; //storing the UID of the tag in readcard
+    array_to_string(readcard, 4, str);
+    StrUID = str;
+  }
+  mfrc522.PICC_HaltA();
+  return 1;
+}
+// --------------------------------------------------------------------
 
-  // wait for a second
-  delay(1000);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-
-   // wait for a second
-  delay(1000);
+void loop() {
+  readsuccess = getid();
+ 
+  if(readsuccess) {
+    Serial.println(StrUID);
+    Serial.println("");   
+  }
 }
