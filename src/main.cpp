b295a88df698001fc9c9808c0c32e55fe67ed7bd
LucasAbitbol <lucasabitbol6@gmail.com>
2022-04-01 14:27:16 +0200

    Projet terminé

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,39 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include "PageIndex.h"
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
-#define SS_PIN D2  //--> SDA / SS is connected to pinout D2
-#define RST_PIN D1  //--> RST is connected to pinout D1
+#define SS_PIN D8  //--> SDA / SS is connected to pinout D
+#define RST_PIN D3  //--> RST is connected to pinout D3
 MFRC522 mfrc522(SS_PIN, RST_PIN);  //--> Create MFRC522 instance.
+
+void MQTTcallback(char* topic, byte* payload, unsigned int length);
 
 #define ON_Board_LED 2
  
-const char* ssid = "Pioneer"; // Enter your WiFi name
-const char* password =  "eb96bbb9"; // Enter WiFi password
+const char* ssid = "Honor 10"; // Enter your WiFi name
+const char* password =  "12345678"; // Enter WiFi password
 const char* mqttServer = "maqiatto.com";
 const int mqttPort = 1883;
-const char* mqttUser = "adrien.peyrouty@e-rekcah.com";
-const char* mqttPassword = "ISIS_IoT_2020";
+const char* mqttUser = "sarah.gros@etud.univ-jfc.com";
+const char* mqttUser2 = "beckerslucas1@gmail.com";
+const char* mqttPassword = "iEnter";
+const char* mqttPassword2 = "azerty";
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
  
 WiFiClient espClient;
+WiFiClient espClient2;
 PubSubClient client(espClient);
+PubSubClient client2(espClient2);
 ESP8266WebServer server(80);
 int readsuccess;
 byte readcard[4];
 char str[32] = "";
 String StrUID;
+
 
 void array_to_string(byte array[], unsigned int len, char buffer[]) {
     for (unsigned int i = 0; i < len; i++)
@@ -52,13 +64,37 @@ int getid() {
     StrUID = str;
   }
   mfrc522.PICC_HaltA();
+  Serial.println(str);
   return 1;
+}
+
+void MQTTcallback(char* topic, byte* payload, unsigned int length) {
+      payload[length] = '\0';
+      String authorisation = String((char*)payload);
+      if (authorisation == "authorized") {
+        Serial.println("Accès autorisé");
+        lcd.init();
+        lcd.backlight();
+        lcd.setCursor(3,1);
+	      lcd.print("Acces autorise");
+        delay(3000);
+        lcd.clear();
+      }
+      if (authorisation == "denied") {
+        Serial.println("Accès refusé");
+        lcd.init();
+        lcd.backlight();
+        lcd.setCursor(4,1);
+	      lcd.print("Acces refuse");
+        delay(3000);
+        lcd.clear();
+      }
 }
  
 void setup() {
- 
+  
   Serial.begin(115200);
- 
+
   WiFi.begin(ssid, password);
   SPI.begin();
   mfrc522.PCD_Init();
@@ -74,20 +110,16 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   Serial.println("Connected to the WiFi network");
-
-
-
-  Serial.println("Please tag a card or keychain to see the UID !");
-  Serial.println("");
  
   client.setServer(mqttServer, mqttPort);
+  client2.setServer(mqttServer, mqttPort);
  
   while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
+    Serial.println("Connecting to Sarah Gros MQTT...");
  
-    if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
+    if (client.connect("marcelle1", mqttUser, mqttPassword )) {
  
-      Serial.println("connected");  
+      Serial.println("Connected to Sarah Gros");  
  
     } else {
  
@@ -97,13 +129,51 @@ void setup() {
  
     }
   }
+
+    while (!client2.connected()) {
+    Serial.println("Connecting to Lucas Beckers MQTT...");
  
+    if (client2.connect("marcelle1", mqttUser2, mqttPassword2 )) {
+ 
+      Serial.println("Connected to Lucas Beckers");  
+      client2.subscribe("beckerslucas1@gmail.com/secureDoor");
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client2.state());
+      delay(2000);
+    }
+      }
+
+  client2.setCallback(MQTTcallback);
+
+  Serial.println("Please tag a card or keychain to see the UID !");
+  Serial.println("");
 }
  
 void loop() {
   client.loop();
-
+  client2.loop();
   if(getid()==1) {
-      client.publish("adrien.peyrouty@e-rekcah.com/test1", StrUID.c_str()); //Topic name
+    if (!client.connected()) {
+      if (client.connect("marcelle1", mqttUser, mqttPassword )) {
+        client.subscribe("sarah.gros@etud.univ-jfc.com/iEnter");
+        client.publish("sarah.gros@etud.univ-jfc.com/iEnter", StrUID.c_str()); //Topic name
+      } else {
+        Serial.print("failed with state ");
+        Serial.print(client.state());
+        delay(2000);
+      }
+    }
+  }
+  if (!client2.connected()) {
+    if (client2.connect("marcelle1", mqttUser2, mqttPassword2 )) {
+      client2.subscribe("beckerslucas1@gmail.com/secureDoor");
+      client2.loop();
+    }
+    else {
+      Serial.print("Echec");
+    }
   }
 }
